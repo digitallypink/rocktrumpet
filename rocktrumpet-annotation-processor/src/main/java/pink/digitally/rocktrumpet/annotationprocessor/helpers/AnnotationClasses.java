c952be63cba40035e53a7e7fc5c1b7cc46996842
UjuE <UjuE@users.noreply.github.com>
2020-03-08 04:59:33 +0000

    Enabling travis

--- rocktrumpet-annotation-processor/src/main/java/pink/digitally/rocktrumpet/annotationprocessor/helpers/AnnotationClasses.java
+++ rocktrumpet-annotation-processor/src/main/java/pink/digitally/rocktrumpet/annotationprocessor/helpers/AnnotationClasses.java
@@ -1,30 +1,33 @@
 package pink.digitally.rocktrumpet.annotationprocessor.helpers;
 
-import java.io.File;
-import java.net.URL;
-import java.util.Arrays;
-import java.util.Optional;
+import pink.digitally.rocktrumpet.annotations.ClassDescription;
+import pink.digitally.rocktrumpet.annotations.Heading;
+import pink.digitally.rocktrumpet.annotations.MethodDescription;
+import pink.digitally.rocktrumpet.annotations.PageTitle;
+import pink.digitally.rocktrumpet.annotations.Summary;
+
+import java.util.List;
 import java.util.stream.Collectors;
+
+import static java.util.Arrays.asList;
 
 public class AnnotationClasses {
 
-    private static final String STRING_THAT_WILL_BE_UNMATCHED = "[-]{100}";
+    private static final List<Class<?>> ANNOTATION_CLASSES =
+            asList(
+                    ClassDescription.class,
+                    Heading.class,
+                    MethodDescription.class,
+                    PageTitle.class,
+                    Summary.class
+            );
 
     private AnnotationClasses() {
     }
 
     public static String annotationClassesRegex() {
-        final URL resource = Thread.currentThread().getContextClassLoader()
-                .getResource("pink/digitally/rocktrumpet/annotations");
-        if (resource != null) {
-            final File file = new File(resource.getFile());
-            final String[] classNames = file.list((dir, name) -> name.endsWith(".class"));
-            return Optional.ofNullable(classNames).map(theClassNames -> Arrays.stream(theClassNames)
-                    .map(className -> className.replaceAll(".class", ""))
-                    .map(className -> "[\\n]?@" + className + "[\\s]?.*[\\n]?")
-                    .collect(Collectors.joining("|")))
-                    .orElse(STRING_THAT_WILL_BE_UNMATCHED);
-        }
-        return STRING_THAT_WILL_BE_UNMATCHED;
+        return ANNOTATION_CLASSES.stream()
+                .map(className -> "[\\n]?@" + className.getSimpleName() + "[\\s]?.*[\\n]?")
+                .collect(Collectors.joining("|"));
     }
 }
